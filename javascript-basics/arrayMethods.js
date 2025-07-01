ca2a9242a5e3c1b0edfffae8ac35b67ca07fd73f
hari babu <haribabu.c535@gmail.com>
2025-07-01 22:21:26 +0530

    strin&arraymethods

---
+++ javascript-basics/arrayMethods.js
@@ -0,0 +1,74 @@
+
+
+// let a=["hari","manoj","amar","bharath"]
+
+// // length
+// console.log(arr.length)
+
+// // bracket notation
+// console.log(arr[0])
+// console.log(arr[12])
+
+// // at
+// console.log(arr.at(-1))
+// console.log(arr.at(1))
+// console.log(arr.at(12))
+
+// // concat
+// let arr=["abc"]
+// console.log(arr.concat(["charan","pavan"]).concat(["cde"]).length)
+// console.log(arr)
+
+// copyWithIn
+
+
+// let a=["hari","manoj","amar","bharath"]
+// console.log(a.copyWithin(0,3))
+// console.log(a.copyWithin(3,0,1))
+
+// fill
+
+// let a=[1,2,3,4,5]
+// console.log(a.fill(0))
+// console.log(a)
+// console.log(a.fill(1))
+// console.log(a.fill(2,3))
+// console.log(a.fill(3,1,2))
+
+// let arr= new Array(5).fill(0)
+// let arr2=[]
+// console.log(arr,arr2)
+
+// let arr=[[1,2],[1,[1,[1,[3,[4,5,6]]]]]]
+
+// // flat
+// console.log(arr.flat(Infinity),arr) // depth
+
+
+// let a=["amar","hari","manoj","amar"]
+// console.log(a.includes("hari",1))
+
+// console.log(a.indexOf("hari",3))
+// console.log(a.lastIndexOf("amar",2))
+
+let a=[1,2,3,4]
+
+// console.log(a.join())
+// console.log(a.join(""))
+// console.log(a.join("-"))
+
+// console.log(a.reverse())
+
+// slice
+
+console.log(a.slice(1))
+console.log(a.slice(1,3))
+console.log(a.slice(-1))
+console.log(a.slice(1,-1))
+
+
+
+
+
+
+
--- javascript-basics/stringMethod.js
+++ javascript-basics/stringMethod.js
@@ -3,6 +3,7 @@
 
 
 // length property
+// let stri="12345"
 
 // console.log(str.length);
 
@@ -32,7 +33,7 @@
 // // concat 
 // let a="abcde"
 // let b="something"
-// console.log("abcde".concat("fgh"),a+"fgh",a.concat(b),a+b)
+// console.log("abcde".concat("fgh",1,"123"),a+"fgh",a.concat(b),a+b)
 
 // console.log("abcde".at(-1))
 // console.log("ksdjflkjsdf".charAt(1))
@@ -40,21 +41,82 @@
 
 // startswith
 
-// console.log(str.startsWith("a"),str.startsWith("abc"))
+// let str="abcsfasfdasd"
+
+// console.log(str.startsWith("a"),str.startsWith("bc"))
 // console.log(str.startsWith("b",1),str.startsWith("cd",2))
 
-// endsWith
+// // endsWith
 
-// console.log(str.endsWith("e"),str.endsWith("ade"))
+// console.log(str.endsWith("e"),str.endsWith("de"))
 // console.log(str.endsWith("c",3),str.endsWith("bc",3))
 
-let str="abcde"
-console.log(str.includes("ab"))
+// console.log(str.includes("f",8))
 
 // if(email.includes("@") || email.includes(".")){
 //     console.log("invalid email please a valid email")
 // }
 
+
+// indexOf
+
+// let str="abfcdefdd"
+
+// console.log(str.indexOf("f"))
+// console.log(str.indexOf("d",4))
+// console.log(str.indexOf("x"))
+
+// // lastIndexOf
+
+// console.log(str.lastIndexOf("f"))
+// console.log(str.lastIndexOf("1"))
+// console.log(str.lastIndexOf("f",3))
+
+// let a="plese enter the password"
+
+// console.log(a.padStart(4,"*"))
+
+// console.log(a.padEnd(a.length+5,"."),a.length)
+
+// let first="123"
+// console.log(first.repeat(10))
+// console.log(first.repeat(0),"")  
+
+// let a="123453"
+
+// console.log(a.replace("3","123"))
+// // regex
+// console.log(a.replace(/3/g,"abc"))
+
+// console.log(a.replaceAll("3","123"))
+
+// let a="01234"
+// console.log(a.slice(2))
+// console.log(a.slice(1,3)) // index based- doesn't includes 3rd index 
+// console.log(a.slice(0,-1))
+// console.log(a.slice(0))
+
+// let a="01234"
+// console.log(a.substring(1,3)) //negative index doesn't work
+// console.log(a.substr(1,3))
+
+
+// let str="red,blue,green"
+// console.log(str.split(""))
+// console.log(str.split())
+// console.log(str.split("s"),str.split("n"),str.split(","))
+
+// let a="abc"
+// console.log(a.toLowerCase())
+// console.log(a.toUpperCase())
+// console.log(a.slice(1).toUpperCase())
+
+
+// let a="   123   "
+// console.log(a.trimStart())
+// console.log(a.trimEnd())
+// console.log(a.trimStart().trimEnd())
+// console.log(a.trim())
 
 
 
